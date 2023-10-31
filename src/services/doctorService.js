afe0efb832910b66988575a757a8e0a35b4188a1
POUCHENVN\web.vandu <web.vandu@pyv.com.vn>
2023-10-31 14:12:13 +0700

    Update

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -8,7 +8,7 @@ let getTopDoctorHome = (limitInput)=>{
                 where : {roleId: 'R2'},
                 order : [['createdAt','DESC']],
                 attributes:{
-                    exclude: ['password','image']
+                    exclude: ['password']
                 },
                 include:[
                     {model: db.Allcode, as: 'positionData', attributes:['valueEn','valueVi']},
