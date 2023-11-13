1643b35df0e1e18f28bf3dc9d2c5e5fb932e2570
POUCHENVN\web.vandu <web.vandu@pyv.com.vn>
2023-11-13 15:59:02 +0700

    Update

--- src/route/web.js
+++ src/route/web.js
@@ -28,6 +28,7 @@ let initWebRoutes = (app) => {
 
     router.get('/api/allcode', userController.getAllcode);
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome);
+    router.get('/api/get-all-doctors', doctorController.getAllDoctors);
 
     return app.use('/', router);
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -29,6 +29,27 @@ let getTopDoctorHome = (limitInput)=>{
     })
 }
 
+let getAllDoctors = ()=>{
+    return new Promise(async (resolve,reject)=>{
+        try{
+            let doctors = await db.User.findAll({
+                where : {roleId : 'R2'},
+                attributes:{
+                    exclude: ['password','image']
+                },
+            })
+
+            resolve({
+                errCode: 0,
+                data: doctors
+            })
+        }catch(e){
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
-    getTopDoctorHome : getTopDoctorHome
+    getTopDoctorHome : getTopDoctorHome,
+    getAllDoctors : getAllDoctors
 }
