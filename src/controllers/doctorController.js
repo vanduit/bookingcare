ac690f3fc32d936c86f04bb7a60af42ccc638ba6
POUCHENVN\web.vandu <web.vandu@pyv.com.vn>
2023-11-15 16:22:36 +0700

    Update

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -41,8 +41,21 @@ let postInforDoctor = async (req,res)=>{
     }
 }
 
+let getDetailDoctorById = async (req,res)=>{
+    try{
+        let infor = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(infor);
+    }catch(e){
+        return res.status(200).json({
+            errCode : -1,
+            errMessage: 'Error from the server'
+        })
+    }
+}
+
 module.exports = {
     getTopDoctorHome:getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     postInforDoctor: postInforDoctor,
+    getDetailDoctorById:getDetailDoctorById,
 }
